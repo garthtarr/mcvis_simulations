076296176879ba5d82ad199d34822eb38b11ea4a
Kevin Wang <kevin.wang@sydney.edu.au>
2019-12-10 17:24:50 +1100

    upd chen.R

---
+++ chen.R
@@ -0,0 +1,29 @@
+nExp = 1000
+p = 4
+
+taudf = matrix(0, nrow = nExp, ncol = p)
+vifdf = matrix(0, nrow = nExp, ncol = p)
+
+for (l in 1:nExp){
+  w = matrix(rnorm(p*n,10,100),n,p)
+  x = sqrt(1-gamma^2)*w + gamma*w[,p]
+  z = apply(x, 2, function(col){
+    res = (col - mean(col))/sd(col)
+    # tmp = col - mean(col)
+    # res = tmp/sqrt(sum(tmp^2))
+    return(res)
+  })
+  apply(z, 2, mean)
+  apply(z, 2, var)
+  ztz_inv = solve(t(z) %*% z)
+  (vif = diag(ztz_inv))
+  svd_obj = svd(ztz_inv)
+  (taus = svd_obj$d)
+  (svd_obj$u^2 %*% taus) ## Equal to vif by theory
+  
+  
+  taudf[l,] = taus[p:1]
+  vifdf[l,] = vif    
+  # taudf[l,] = taus
+  # vifdf[l,] = sort(vif)
+}
